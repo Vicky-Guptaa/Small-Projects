be79dae62f3c667adf31f553003fb26b19abe253
Vicky Gupta <vickyguptaa7@gmail.com>
2022-04-03 15:25:48 +0530

    infinity scroll completed

--- 13.Infinity Scroll/src/js/app.js
+++ 13.Infinity Scroll/src/js/app.js
@@ -1,17 +1,31 @@
 //https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
 
+// Initial Loader Shown Till Images Are Not Loaded
 const loader=document.querySelector(".loader");
 const imageContainer=document.querySelector(".image-container");
 
+
 let imagesArray=[];
 
-const count=10;
+//amount of images fetches from the api
+let imagesCount=5;
+
+// my api key
 const apiKey='NUsQ1f8an9GQAmEs7EKkUvYNRMV_Dw65sfAsIUlNgtQ';
 
-const apiUrl=`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const apiUrl=`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${imagesCount}`;
 
+// this is used for loading all the images which are fetched when all loaded it bcom true
+let isReady=false;
 
-(async function getImages(){
+// total images we get from the api
+let totalImages=0;
+
+// images loaded from the api
+let imagesLoaded=0;
+
+// fetching the images 
+async function getImages(){
     try{
         const response=await fetch(apiUrl);
         imagesArray=await response.json();
@@ -21,9 +35,55 @@ const apiUrl=`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=
         console.log("error : ",error);
     }
 
-})()
+}
+getImages();
 
+
+// keep the count images loaded 
+function imageLoaded(){
+    imagesLoaded++;
+    
+    // when all images loaded
+    if(imagesLoaded===totalImages)
+    {
+        // initialize back to zero 
+        imagesLoaded=0;
+
+        //hide the loader
+        loader.classList.add("hidden");
+
+        // now all images loaded
+        isReady=true;
+
+        // first our images count is small bcoz to load the content faster when we open and after that first loading we increase the count so we can get more images from the api
+        imagesCount=20;
+    }
+}
+
+
+// we get images only when we are at bottom for infinite scroll
+window.addEventListener("scroll",(e)=>{
+
+    // note : do not use h-100vh bcoz the body.offsetHeight does not give whole size it give only viewport height
+    if(window.innerHeight+window.scrollY>=document.body.offsetHeight-1000&&isReady)
+    {
+        // we are now start fetch image again so make it false
+        isReady=false;
+
+        // console.log("ScrollY",window.scrollY);
+        // console.log("innerHiehgt",window.innerHeight);
+        // console.log("offsetHeight",document.body.offsetHeight);
+        // console.log("load more");
+        // console.log("isReady=",false);
+        getImages();
+    }
+})
+
+// use to show the images which are fetch from the api by manipulating the dom
 function displayImages(){
+    
+    totalImages=imagesArray.length;
+
     imagesArray.forEach((image)=>{
         const imageWrapper=document.createElement("div");
         setAttribute(imageWrapper,{
@@ -43,7 +103,10 @@ function displayImages(){
             title:image.alt_discription,
             class:'img-style'
         })
-        
+
+        // used to know if the images are loaded fully or not
+        img.addEventListener("load",imageLoaded);
+
         //Put The Anchor Tag To The Div Which Is The Image Wrapper
         imageWrapper.appendChild(anchor);
         
@@ -55,6 +118,8 @@ function displayImages(){
     })
 }
 
+
+// help to setup the key value pair for the attribute
 function setAttribute(element,attributes){
     for(const key in attributes)
     {
